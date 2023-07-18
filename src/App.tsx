4c7912aea9d8334fa16449dd688a6bbe816adc21
Evans-mutuku <evansnyamai98@gmail.com>
2023-07-18 20:06:48 +0300

    started the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+
+
+  return (
+    <main>
+      <h1 className='text-red-500 font-bold text-3xl'>Hello world</h1>
+    </main>
+  )
+}
+
+export default App
